042cb9180a50e5876821ea2863bba4579668f99f
edi <analisticsdataprojetos@gmail.com>
2025-03-31 16:49:21 -0300

    Cadastros

--- app/Resources/Language.php
+++ app/Resources/Language.php
@@ -10,6 +10,8 @@ class Language implements ILanguage{
    
     
     
+
+    
     
     private $languages = [
         'pt-br' => [
--- new-account.php
+++ new-account.php
@@ -1,5 +1,9 @@
 <?php
 
+
+
+
+
 require_once __DIR__.'/twig_boot.php';
  
 
